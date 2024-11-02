437447b1790d4bafba74090c7684148a557b262b
beak2825 <113315384+beak2825@users.noreply.github.com>
2024-11-02 16:16:26 -0400

    Update ver.js

--- ver.js
+++ ver.js
@@ -3,3 +3,6 @@ var ver = "ver_nov3_2024";
 
 // Update message for the user
 var msg = "You are on an old version. A new version is available. Added Block Game";
+
+// Image URL to display above the message
+var img = "https://raw.githubusercontent.com/beak2825/special-bassoon/refs/heads/main/image.png";
