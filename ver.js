c9048aa03eae298bf1a6c9f3e8967d138918825c
beak2825 <113315384+beak2825@users.noreply.github.com>
2024-11-02 16:23:05 -0400

    Update ver.js

--- ver.js
+++ ver.js
@@ -1,8 +1,8 @@
 // Latest version of the game
-var ver = "ver_nov3_2024";
+var ver = "ver_nov2_2024";
 
 // Update message for the user
-var msg = "You are on an old version. A new version is available. Added Block Game";
+var msg = "You are on an old version. A new version is available. Added FNAF + Added the Version Checker";
 
 // Image URL to display above the message
 var img = "https://raw.githubusercontent.com/beak2825/special-bassoon/refs/heads/main/image.png?token=$(date%20+%s)";
