5d59672de31025bdc39738170d471f89f096dd5e
beak2825 <113315384+beak2825@users.noreply.github.com>
2024-11-07 04:07:50 -0500

    Update ver.js

--- ver.js
+++ ver.js
@@ -1,8 +1,8 @@
 // Latest version of the game
-var ver = "ver_nov3_2024";
+var ver = "ver_nov7_2024";
 
 // Update message for the user
-var msg = "You are on an old version. A new version is available. Added FNAF + Added the Version Checker + Many More";
+var msg = "You are on an old version. A new version is available. Added Fortnite + Rocket Soccer League + Penguin Diner + Minecraft";
 
 // Image URL to display above the message
 var img = "https://raw.githubusercontent.com/beak2825/special-bassoon/refs/heads/main/image.png?token=$(date%20+%s)";
