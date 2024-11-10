604bfa6e4f2043dc83c83c44ae21b893f08de26e
beak2825 <113315384+beak2825@users.noreply.github.com>
2024-11-10 02:54:19 -0500

    Update ver.js

--- ver.js
+++ ver.js
@@ -1,8 +1,8 @@
 // Latest version of the game
-var ver = "ver_nov8_2024";
+var ver = "ver_nov10_2024";
 
 // Update message for the user
-var msg = "You are on an old version. A new version is available. Added Fortnite + Rocket Soccer League + Penguin Diner + Minecraft + SuperHot + Tomb Of The Mask + Newscripts";
+var msg = "You are on an old version. A new version is available. Added Tomb Of The Mask + Real Flight Simulator + AdVenture Capitalist + Infiltrating The Airship + Kawai Run 2 + Soccer Physics + ALSO Some games have a speedhack script enabled, press 1 for 1x, 2 for 0.3x, 3 for 0.7x, 4 for 2x, 5 for 10x, 6 for 0x";
 
 // Image URL to display above the message
 var img = "https://raw.githubusercontent.com/beak2825/special-bassoon/refs/heads/main/image.png?token=$(date%20+%s)";
