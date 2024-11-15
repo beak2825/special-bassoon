6885eb4ff7294df1fc61f112414029cf3e23cfcd
beak2825 <113315384+beak2825@users.noreply.github.com>
2024-11-14 20:47:43 -0500

    Update gamesData2.js

--- scripts/gamesData2.js
+++ scripts/gamesData2.js
@@ -1,6 +1,7 @@
 // Formatting: {GAMEPATH}:{GAMENAME}:{THUMBNAMIL}
 // The Links are autoadded by the generate boxes script. Including initial game link and initial images link.
 const games = [
+  "block-eat-block:Block Eat Block:block-eat-block.png",
   "soccerphysics:Soccer Physics (Takes 20sec to load, not laggy):soccer_physics.png",
   "mc:Minecraft (Eaglercraft):mc.png",
   "fluidsim:Fluid Simulator:fluidsim.png",
