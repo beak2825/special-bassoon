9baddf48d4bf32a23358a9c22bf2113ae9e59222
beak2825 <SUPERJOHNYTPER3@GMAIL.COM>
2024-11-14 23:18:05 -0500

    HouseOfHazards FINALE + homesites update + gamesData

--- scripts/gamesData3.js
+++ scripts/gamesData3.js
@@ -2,6 +2,7 @@
 // The Links are autoadded by the generate boxes script. Including initial game link and initial images link.
 const games = [
   "flash-games/1on1soccer:1 on 1 Soccer:1on1soccer.png",
+  "unity-games/houseofhazards:House Of Hazards:HouseOfHazards.png",
   "unity-games/block-eat-block:Block Eat Block:block-eat-block.png",
   "unity-games/soccerphysics:Soccer Physics (Takes 20sec to load, not laggy):soccer_physics.png",
   "minecraft-eaglercraft:Minecraft (Eaglercraft):mc.png",
