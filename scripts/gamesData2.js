8a505aeef00fcb55694c5df38187546fab3fa96c
beak2825 <113315384+beak2825@users.noreply.github.com>
2024-11-14 20:19:47 -0500

    Update name for soccer_physics. Home site added incase for future editing

--- scripts/gamesData2.js
+++ scripts/gamesData2.js
@@ -1,7 +1,7 @@
 // Formatting: {GAMEPATH}:{GAMENAME}:{THUMBNAMIL}
 // The Links are autoadded by the generate boxes script. Including initial game link and initial images link.
 const games = [
-  "soccerphysics:Soccer Physics:soccer_physics.png",
+  "soccerphysics:Soccer Physics (Takes 20sec to load, not laggy):soccer_physics.png",
   "mc:Minecraft (Eaglercraft):mc.png",
   "fluidsim:Fluid Simulator:fluidsim.png",
   "doom:Doom Emulator:doom.png",
