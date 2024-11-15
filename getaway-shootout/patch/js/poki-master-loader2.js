902b3a7258991bb4e1c3e243378b44d2e84c3e23
beak2825 <SUPERJOHNYTPER3@GMAIL.COM>
2024-11-15 02:40:21 -0500

    .

--- getaway-shootout/patch/js/poki-master-loader2.js
+++ getaway-shootout/patch/js/poki-master-loader2.js
@@ -5,11 +5,11 @@ var scripts = document.getElementsByTagName("script")
   , loaders = {
     unity: "unity.js",
     "unity-beta": "unity-beta.js",
-    "unity-2020": "unity-2020.js"
+    "unity-2020": "unity-20201.js"
 };
 if (0 <= window.location.href.indexOf("pokiForceLocalLoader") && (loaders.unity = "/unity/dist/unity.js",
 loaders["unity-beta"] = "/unity-beta/dist/unity-beta.js",
-loaders["unity-2020"] = "/unity-2020/dist/unity-2020.js",
+loaders["unity-2020"] = "/unity-2020/dist/unity-20201.js",
 root = "/loaders"),
 !window.config)
     throw Error("window.config not found");
