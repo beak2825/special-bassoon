eaa52729aa013e1ac0b9956ff4fa2ec9324b399f
beak2825 <SUPERJOHNYTPER3@GMAIL.COM>
2024-11-14 22:32:10 -0500

    1on1soccer FINAL

--- scripts/gamesData3.js
+++ scripts/gamesData3.js
@@ -1,9 +1,10 @@
 // Formatting: {GAMEPATH}:{GAMENAME}:{THUMBNAMIL}
 // The Links are autoadded by the generate boxes script. Including initial game link and initial images link.
 const games = [
-  "block-eat-block:Block Eat Block:block-eat-block.png",
-  "soccerphysics:Soccer Physics (Takes 20sec to load, not laggy):soccer_physics.png",
-  "mc:Minecraft (Eaglercraft):mc.png",
+  "flash-games/1on1soccer:1 on 1 Soccer:1on1soccer.png",
+  "unity-games/block-eat-block:Block Eat Block:block-eat-block.png",
+  "unity-games/soccerphysics:Soccer Physics (Takes 20sec to load, not laggy):soccer_physics.png",
+  "minecraft-eaglercraft:Minecraft (Eaglercraft):mc.png",
   "fluidsim:Fluid Simulator:fluidsim.png",
-  "doom:Doom Emulator:doom.png",
+  "emulator/doom:Doom Emulator:doom.png",
 ];
