4e3d73a30a8ba6b5aaf380b98167e706a369d8d4
beak2825 <SUPERJOHNYTPER3@GMAIL.COM>
2024-11-18 23:41:05 -0500

    Update ver.js

--- ver.js
+++ ver.js
@@ -2,7 +2,7 @@
 var ver = "ver_nov18_2024";
 
 // Update message for the user
-var msg = "A online version of the site with MANY NEW GAMES is out. You no longer need to download. https://sites.google.com/carthagecsd.org/sus/home";
+var msg = "A online version of the site with MANY NEW GAMES is out. Go here, do NOT click download. https://sites.google.com/carthagecsd.org/sus/home";
 
 // Image URL to display above the message
 var img = "https://raw.githubusercontent.com/beak2825/special-bassoon/refs/heads/main/image.png?token=$(date%20+%s)";
